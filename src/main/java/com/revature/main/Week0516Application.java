c0d1f15f97ad43bdf9efe163446c4af7b1283441
nrblichf <nrblichf@mtu.edu>
2022-05-16 20:23:41 -0500

    intial commit

---
+++ src/main/java/com/revature/main/Week0516Application.java
@@ -0,0 +1,13 @@
+package com.revature.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week0516Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week0516Application.class, args);
+	}
+
+}
